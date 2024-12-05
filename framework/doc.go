2ab0461acce9e51ef7194b7c9674be51ccbdc1e5
Jennifer Power <barnabei.jennifer@gmail.com>
2024-12-05 18:15:41 -0500

    chore: bumps module to v2 and adds initial package structure
    Signed-off-by: Jennifer Power <barnabei.jennifer@gmail.com>

---
+++ framework/doc.go
@@ -0,0 +1,7 @@
+/*
+ Copyright 2024 The OSCAL Compass Authors
+ SPDX-License-Identifier: Apache-2.0
+*/
+
+// Package framework contain the high-level plugin workflow logic.
+package framework
---
+++ providers/doc.go
@@ -0,0 +1,7 @@
+/*
+ Copyright 2024 The OSCAL Compass Authors
+ SPDX-License-Identifier: Apache-2.0
+*/
+
+// Package providers contains interface that define supported plugin protocols.
+package providers
